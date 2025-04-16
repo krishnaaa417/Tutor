63c790d836f07e61e0008ca14951e22cf8cf6a24
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-16 22:22:41 +0530

    Pattern2

---
+++ Tutor/Assignment/Pattern2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Assignment
+{
+    internal class Pattern2
+    {
+        static void Main(string[] args)
+        {
+            Pattern2.LeftAlign();
+        }
+
+        public static void LeftAlign()
+        {
+            Console.WriteLine("please enter the rows number");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = i; k >= 1; k--)
+                {
+                    Console.Write(k);
+                }
+
+                for(int s = 2; s <= i;  s++)
+                {
+                    Console.Write(s);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
