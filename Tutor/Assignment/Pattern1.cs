e670516e3e599cfed4a736c6b326a3de317c40e5
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-16 22:09:18 +0530

    Assignments

---
+++ Tutor/Assignment/Pattern1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Assignment
+{
+    internal class Pattern1
+    {
+        static void Main(string[] args)
+        {
+            Pattern1.HallowSquarePattern_Approach1();
+        }
+
+
+        //******
+        //*    *
+        //*    *
+        //*    *
+        //*    *
+        //******
+
+
+        public static void HallowSquarePattern_Approach1()
+        {
+            //Console.WriteLine("please enter the rowss");
+            //int rows = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= rows; i++)
+            //{
+            //    //for (int star = 1; star <= rows; star++)
+            //    //{
+            //    //    Console.Write('*');
+            //    //}
+            //    //Console.WriteLine();
+
+
+            //    for (int star = 1; star <= rows; star++)
+            //    {
+            //        if (i == 1 || star == 1 || i == rows || star == rows)
+            //        {
+            //            Console.Write('*');
+            //        }
+            //        else
+            //        {
+            //            Console.Write(' ');
+            //        }
+            //    }
+
+            //    Console.WriteLine();
+
+            //    //for (int j = 1; j <= rows; j++)
+            //    //{
+            //    //   Console.Write('*');
+            //    //}
+            //    //Console.WriteLine();
+        }
+
+        public static void ApproachTwo()
+        {
+            int rows = 5;
+            for (int i = 1; i <= rows; i++)
+            {
+                
+            }
+        }
+    }
+}
+
