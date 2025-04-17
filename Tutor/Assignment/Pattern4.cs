d42bd17ffa12642ed79b061fe8c46fb8d29ee5eb
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-17 10:28:31 +0530

    some more number patterns

---
+++ Tutor/Assignment/Pattern4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Assignment
+{
+    internal class Pattern4
+    {
+        static void Main(string[] args)
+        {
+            Pattern4.NumberPatter1();
+        }
+
+        public static void NumberPatter1()
+        {
+            Console.WriteLine("please enter the row number");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Tutor/Assignment/Pattern5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Assignment
+{
+    internal class Pattern5
+
+
+
+    {
+        static void Main(string[] args)
+        {
+            Pattern5.Number2();
+        }
+
+        public static void Number2()
+        {
+            int rows = 7;
+            int number = 1;
+
+            for (int i = 1; i <= rows-1; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(number+++" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
