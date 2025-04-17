370ebcaa720769957c4bcf7154d2f7ac90ed725e
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-17 07:56:39 +0530

    Daily Problem completed

---
+++ Tutor/Assignment/Pattern3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Assignment
+{
+    internal class Pattern3
+    {
+        static void Main(string[] args)
+        {
+            Pattern3.Pattern3ass();
+        }
+
+        public static void Pattern3ass()
+        {
+            int rows = 7;
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 1; k <= rows-1; k++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
