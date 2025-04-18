64757f7315f94ae6f1daf414f61d95014fc36a13
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-18 09:11:23 +0530

    added two more patterns

---
+++ Tutor/Assignment/Pattern7.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Assignment
+{
+    internal class Pattern7
+    {
+        static void Main(string[] args)
+        {
+            Pattern7.starPattern();
+        }
+        //     *
+        //    ***
+        //   *****
+        //  *******
+        // *********
+        //***********
+        // *********
+        //  *******
+        //   *****
+        //    ***
+        //     *
+        public static void starPattern()
+        {
+            int rows = 9;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = rows - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= rows - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int star = 1; star <= 2 * i - 1; star++)
+                {
+                    Console.Write('*');
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
+
+
+
+
---
+++ Tutor/Pattern8.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor
+{
+    internal class Pattern8
+    {
+        static void Main(string[] args)
+        {
+            Pattern8.StarPattern();
+        }
+
+        public static void StarPattern()
+        {
+            int rows = 6; // Half the pattern height
+
+            // Upper Triangle
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int star = 1; star <= i; star++)
+                {
+                    Console.Write("*");
+                }
+                for (int spaces = 1; spaces <= (rows - i) * 2; spaces++)
+                {
+                    Console.Write(" ");
+                }
+                for (int star = 1; star <= i; star++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            // Middle Rows
+            for (int i = 1; i <= rows * 2; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            for (int i = 1; i <= rows * 2; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+            // Lower Triangle
+            for (int i = rows; i >= 1; i--)
+            {
+                for (int star = 1; star <= i; star++)
+                {
+                    Console.Write("*");
+                }
+                for (int spaces = 1; spaces <= (rows - i) * 2; spaces++)
+                {
+                    Console.Write(" ");
+                }
+                for (int star = 1; star <= i; star++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
