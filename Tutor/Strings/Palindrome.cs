588ee60ae17e646ec816f1d27b3b8c13142e6654
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-26 10:39:37 +0530

    how to find a given string is a palindrome or not?

---
+++ Tutor/Strings/Palindrome.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Strings
+{
+    internal class Palindrome
+    {
+        static void Main(string[] args)
+        {
+            palindrome();
+        }
+
+        private static void palindrome()
+        {
+            Console.WriteLine("please enter a name");
+            string name = Console.ReadLine();
+            char[] c = name.ToCharArray();
+
+            int left = 0;
+            int right = c.Length - 1;
+
+            while (left < right)
+            {
+                char temp = c[left];
+                c[left] = c[right];
+                c[right] = temp;
+
+                left++;
+                right--;
+            }
+
+            string palidromes = new string(c);
+            if (name == palidromes)
+            {
+                Console.WriteLine("Congrats,your given name is a palindrome:-"+name);
+            }
+            else
+            {
+                Console.WriteLine("sorry bro :-" + name);
+            }
+        }
+
+
+        
+
+    }
+}
