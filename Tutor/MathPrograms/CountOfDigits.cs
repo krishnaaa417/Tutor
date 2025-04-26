0ad0cd5ca8f078cd4d488c6cd23dbfa22bff3a86
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-26 16:55:48 +0530

    Count Of digits in a given number

---
+++ Tutor/MathPrograms/CountOfDigits.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.MathPrograms
+{
+    internal class CountOfDigits
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Count(n)); ;
+        }
+
+        private static int Count(int n)
+        {
+            int count = 0;
+            try
+            {
+                while (n > 0)
+                {
+                    n = n / 10;
+                    count++;
+                }
+            }
+            catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+
+            return count;
+
+
+        }
+    }
+}
