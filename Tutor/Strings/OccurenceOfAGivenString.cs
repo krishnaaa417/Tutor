776d45fc9861903e2cf8c3a85ca3753248902ec0
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-26 12:01:23 +0530

    Occurence of a given charcter in a string

---
+++ Tutor/Strings/OccurenceOfAGivenString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Strings
+{
+    internal class OccurenceOfAGivenString
+    {
+        static void Main(string[] args)
+        {
+            Occurence();
+        }
+
+        private static void Occurence()
+        {
+            string s = Console.ReadLine();
+            char[] c = s.ToCharArray();
+            char k = 'a';
+            int count = 0;
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] == k)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
