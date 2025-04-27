ced33e3c1940ae3c711b7a5481ab72f49890a870
krishnaaa210 <krishnaaa210@gmail.com>
2025-04-27 21:49:00 +0530

    Assignment

---
+++ Tutor/Assignment2/ProductOfNum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Assignment2
+{
+    internal class ProductOfNum
+    {
+        static void Main(string[] args)
+        {
+            int i = 253;
+
+            int product = 1;
+
+            while (i > 1)
+            {
+                int number = i % 10;
+                product = product * number;
+                i = i / 10;
+            }
+            Console.WriteLine(product + "," + "product of numbers");
+
+            
+        }
+    }
+}
