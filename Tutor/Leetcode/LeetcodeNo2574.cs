06d24677551f6e2c9d2253c14cbfb2ee086d6191
krishnaaa210 <krishnaaa210@gmail.com>
2025-05-10 20:34:24 +0530

    Leetcode No:- 2574

---
+++ Tutor/Leetcode/LeetcodeNo2574.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Leetcode
+{
+    internal class LeetcodeNo2574
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 10, 4, 8, 3 };
+            Console.WriteLine(string.Join(",",LeftRightDifference(nums)));
+            //expected output = 0,10,14,22
+        }
+
+        private static int[] LeftRightDifference(int[] nums)
+        {
+            int n = nums.Length;
+            int[] leftsum = new int[n];
+            leftsum[0] = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                leftsum[i] = leftsum[i - 1] + nums[i-1];
+            }
+            return leftsum;
+
+            int[] rightsum = new int[n];
+            rightsum[0] = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+            }
+        }
+    }
+}
