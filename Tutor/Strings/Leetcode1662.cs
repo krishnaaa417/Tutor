f0b6c7c5e5c68f53d9e28d8aa79c37ea19cd7309
krishnaaa210 <krishnaaa210@gmail.com>
2025-05-11 15:54:20 +0530

    Leetcode No 1662

---
+++ Tutor/Strings/Leetcode1662.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Strings
+{
+    internal class Leetcode1662
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Leetcode1662");
+        }
+    }
+}
