675a0ec5027d9b79c52e03ca87f65938cf8e1b45
krishnaaa210 <krishnaaa210@gmail.com>
2025-05-14 23:51:45 +0530

    shuffle the array

---
+++ Tutor/Leetcode/1470Shuffle the Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Leetcode
+{
+    internal class _1470Shuffle_the_Array
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the array size..");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] nums = new int[size];
+            Console.WriteLine("please enter the n value where you need shuffle the ");
+            
+           
+        }
+
+        private static int[] ShuffleTheArray(int[] array)
+        {
+
+        }
+    }
+}
