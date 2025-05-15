70ea7049fa77c33f78d9c8d4b744d77254c98790
krishnaaa210 <krishnaaa210@gmail.com>
2025-05-15 20:27:58 +0530

    reverse the string

---
+++ Tutor/Leetcode/ReverseTheString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Leetcode
+{
+    internal class ReverseTheString
+    {
+        static void Main(string[] args)
+        {
+            char[] c = { 'H', 'e', 'l', 'l', 'o' };
+            for (int i = c.Length - 1; i >= 0; i--)
+            {
+                Console.Write(c[i]);
+            }
+        }
+    }
+}
