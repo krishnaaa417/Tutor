e9b380246f8c451221d5d968aba9a9dcbd0c07e0
krishnaaa210 <krishnaaa210@gmail.com>
2025-05-16 16:17:26 +0530

    Inheritance uploaded

---
+++ Tutor/SystemDesign/LowLevelDesign/Abstractions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.SystemDesign.LowLevelDesign
+{
+    internal class Abstractions
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Abstractions");
+        }
+    }
+}
---
+++ Tutor/SystemDesign/LowLevelDesign/Inheritance.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.SystemDesign.LowLevelDesign
+{
+    internal class Inheritance
+    {
+        static void Main(string[] args)
+        {
+           PW1100G p = new PW1100G();
+            p.EngineValue = 1;
+            p.EngineValue = 2;
+            p.EngineNumber = 123;
+            p.display();
+            PW1500G p2 = new PW1500G();
+            p2.EventId = "123423";
+            p2.show();
+        }
+
+        public class PW1100G
+        {
+            public int EngineNumber;
+            public double EngineValue;
+            public void display()
+            {
+                Console.WriteLine("EngineNumber.."+EngineNumber);
+                Console.WriteLine("EngineValue.."+EngineValue);
+            }
+        }
+
+        public class PW1500G : PW1100G 
+        {
+            public string EventId;
+
+            public void show()
+            {
+                Console.WriteLine("EventId.."+EventId);
+            }
+        }
+    }
+}
