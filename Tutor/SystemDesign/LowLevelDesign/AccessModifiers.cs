90d7a2ddbe0ee60a25925f298d056c0b06ca7d2b
krishnaaa210 <krishnaaa210@gmail.com>
2025-05-16 14:59:44 +0530

    Enacpsultation

--- Tutor/SystemDesign/LowLevelDesign/AccessModifiers.cs
+++ Tutor/SystemDesign/LowLevelDesign/AccessModifiers.cs
@@ -2,12 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Channels;
 using System.Threading.Tasks;
 
 namespace Tutor.SystemDesign.LowLevelDesign
 {
-    internal class AccessModifiers
+    public class AccessModifiers
     {
+        static void Main(string[] args)
+        {
+          Encaps e  = new Encaps();
+            e.SetUserName("admin");
+            e.SetPassword("password");
+
+            Console.WriteLine(e.GetUserName());
+            Console.WriteLine(e.GetPassword());
+        }
+
+    }
+
+    public class Encaps
+    {
+        private string _username;
+        private string _password;
+
+        public void SetUserName(string username)
+        {
+            this._username = username;
+        }
+
+        public string GetUserName()
+        {
+            return _username;
+        }
+
+        public void SetPassword(string password)
+        {
+            this._password = password;
+        }
+        public string GetPassword()
+        {
+            return this._password;
+        }
+    }
+
+    public class Car
+    {
+      public  string company;
+        public int tyres;
+        private double speed;
+
+        public void Display()
+        {
+            Console.WriteLine(company);
+            Console.WriteLine(tyres);
+        }
+
 
     }
 }
