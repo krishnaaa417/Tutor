fca7944589ed597f9551cd7918f1d59093791f2c
krishnaaa210 <krishnaaa210@gmail.com>
2025-05-17 21:25:17 +0530

    ConvertDecimalToBinary

--- Tutor/BitManipulation/ConvertDecimalToBinary.cs
+++ Tutor/BitManipulation/ConvertDecimalToBinary.cs
@@ -10,11 +10,18 @@ namespace Tutor.BitManipulation
     {
         static void Main(string[] args)
         {
-            int i = 45;
-            while (i > 0)
+            Console.WriteLine("please enter a number.");
+            int n = Convert.ToInt32(Console.ReadLine());
+            string b = "";
+
+            while (n >= 1)
             {
-               
+                int x = n % 2;
+                n = n / 2;
+                b = x + b;
             }
+
+            Console.WriteLine(b);
             
         }
     }
