72d0b40cf5d4659ca34e23b2d472b18c6c7daaaa
krishnaaa210 <krishnaaa210@gmail.com>
2025-05-18 19:46:38 +0530

    TwoD array

---
+++ Tutor/Two-DArrays/TwoDArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Two_DArrays
+{
+    internal class TwoDArray
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("haha");
+        }
+    }
+}
