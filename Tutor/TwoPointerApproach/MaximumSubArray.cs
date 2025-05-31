be3e8dc514b1e4078a958b854de0bbe78a4fbe48
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-01 00:30:15 +0530

    brute force approach

---
+++ Tutor/TwoPointerApproach/MaximumSubArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.TwoPointerApproach
+{
+    internal class MaximumSubArray
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { -2,1,-3,4,-1,2,1,-5,4};
+            Console.WriteLine(BruteForceApproach(nums));
+
+        }
+
+        private static int BruteForceApproach(int[] nums)
+        {
+            int n = nums.Length;
+            int maxSum = int.MinValue;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+               sum = sum + nums[i];
+                maxSum = Math.Max(maxSum, sum);
+            }
+            return maxSum;
+        }
+    }
+}
--- Tutor/TwoPointerApproach/SubArraySum.cs
+++ Tutor/TwoPointerApproach/SubArraySum.cs
@@ -10,7 +10,36 @@ namespace Tutor.TwoPointerApproach
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Sub-Array-Sum");
+            int[] nums = { 1, 2, 3 };
+            int k = 3;
+            Console.WriteLine(SubArray(nums,k));
         }
+
+        private static int SubArray(int[] nums, int k)
+        {
+            int n = nums.Length;
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < n; j++)
+                {
+                    sum = sum + nums[j];
+                    if (sum == k)
+                    {
+                        count++;
+                    }
+               
+               }
+               
+            }
+            return count;
+        }
+
+        //private static int SubOptimize(int[] nums, int k)
+        //{
+
+        //}
     }
 }
