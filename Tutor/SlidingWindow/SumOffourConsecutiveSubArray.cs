2b6cce2f725c036471d6620004985cefc6e4187d
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-01 16:43:51 +0530

    sliding window techniques

---
+++ Tutor/SlidingWindow/SumOffourConsecutiveSubArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.SlidingWindow
+{
+    internal class SumOffourConsecutiveSubArray
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 3, 5, 7, 2, 5, 1, 3, 2 };
+            Console.WriteLine(BruteForceApproach(nums));
+        }
+
+        private static int BruteForceApproach(int[] nums)
+        {
+           
+        }
+    }
+}
