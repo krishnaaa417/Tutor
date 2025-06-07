792fe01149184d38c313ae8359043056b2f6cc1d
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-07 12:29:03 +0530

    COunt Pairs : Leet code 2824

---
+++ Tutor/TwoPointerApproach/CountPairs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.TwoPointerApproach
+{
+    internal class CountPairs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Count-Pairs..");
+        }
+    }
+}
