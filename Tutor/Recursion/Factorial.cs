3df89b9b5e7fe1489b0d122168c256ae19482e60
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-08 19:53:53 +0530

    Both BruteForce and Optimized approach are uploaded

--- Tutor/Recursion/Factorial.cs
+++ Tutor/Recursion/Factorial.cs
@@ -10,7 +10,30 @@ namespace Tutor.Recursion
     {
         static void Main(string[] args)
         {
-           
+            int n = 4;
+            Console.WriteLine(Recursion(n));
+        }
+
+        private static int BruteForceApproach(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+            return fact;
+        }
+
+        private static int Recursion(int n)
+        {
+            if (n == 1) return 1;
+            return (n * Recursion(n - 1));
         }
     }
 }
+/*
+ * 5 -> 5*4
+------ 20*3
+60*2
+120*1 120
+ */
