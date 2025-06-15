9c16e34e83718c46066ae381ef86472f5bb8b102
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-15 17:06:18 +0530

    SlidingWindowBruteForceApproach

---
+++ Tutor/SlidingWindow/MaxSumInSubArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.SlidingWindow
+{
+    internal class MaxSumInSubArray
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, 5, 4, 8, 1, 2, 4, 5, 6 };
+            Console.WriteLine(BruteForceApproach(n));
+        }
+
+        private static int BruteForceApproach(int[] arr)
+        {
+          
+           int n = arr.Length;
+            int subarray = 3;
+
+            int maxsum = 0;
+
+            for (int i = 0; i <= n - subarray; i++)
+            {
+                int sum = 0;
+
+                for (int j = i; j < i + subarray; j++)
+                {
+                    sum += arr[j];
+                }
+
+                maxsum = Math.Max(maxsum, sum);
+            }
+            return maxsum;
+           
+
+           
+           
+        }
+    }
+}
