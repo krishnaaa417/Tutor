0d32a4e632cced9c2aeae8072947f4399981b35f
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-19 09:36:39 +0530

    LeetCode problem : 121

---
+++ Tutor/Leetcode/BestTimeToBuyStockAndSellStock.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Leetcode
+{
+    internal class BestTimeToBuyStockAndSellStock
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 7, 1, 5, 3, 6, 4 };
+            Console.WriteLine(OptimisedSolution(nums));
+        }
+
+        private static int OptimisedSolution(int[] prices)
+        {
+            int n = prices.Length;
+            // first I need print min value
+            int min = prices[0];
+            int max = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (prices[i] > min)
+                {
+                    max = Math.Max(max, prices[i]-min);
+                }
+
+                min = Math.Min(min, prices[i]);
+
+                
+            }
+            return max;
+        }
+    }
+}
