2dc3dae63cfd3c5f104ffad8434ccd34b612a4fb
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-21 22:42:28 +0530

    Stack problems uploaded

---
+++ Tutor/Stacks/Temparature.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Stacks
+{
+    internal class Temparature
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 73, 74, 75, 71, 69, 72, 76, 73 };
+            int[] result = BruteForceApproach(nums);
+            foreach (int i in result)
+            {
+                Console.WriteLine(i+",");
+            }
+        }
+
+        private static int[] BruteForceApproach(int[] nums)
+        {
+            int n = nums.Length;
+            int[] result = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (nums[i] < nums[j])
+                    {
+                        result[i] = j - i;
+                        break;
+
+                    }
+                  
+                }
+            }
+            return result;
+        }
+
+        
+    }
+}
---
+++ Tutor/Stacks/ValidParenthsis.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Stacks
+{
+    internal class ValidParenthsis
+    {
+        static void Main(string[] args)
+        {
+            string s = "()[]";
+            Console.WriteLine("Valid-Parenthsis.."+OptimizedApproach(s));
+        }
+
+        private static bool BruteForceApproach(String s)
+        {
+            while (s.Contains("()") || s.Contains("[]") || s.Contains("{}"))
+            {
+               s = s.Replace("()", "").Replace("[]", "").Replace("{}", "");
+            }
+
+            return s.Length == 0;
+            
+        }
+
+        private static bool OptimizedApproach(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+               char c = s[i];
+
+                if (c == ')' && stack.Count > 0 && stack.Peek() == '(')
+                {
+                    stack.Pop();
+                }
+                else if (c == ']' && stack.Count > 0 && stack.Peek() == '[')
+                {
+                    stack.Pop();
+                }
+              else  if (c == '}' && stack.Count > 0 && stack.Peek() == '{')
+                {
+                    stack.Pop();
+                }
+               else  if (c == ']' && stack.Count > 0 && stack.Peek() == '[')
+                {
+                    stack.Push(c);
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
