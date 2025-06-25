4b311fc21206f3f39e541c549c2b1827b452e663
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-25 22:36:47 +0530

    BruteForceApproach

--- Tutor/Arrays/FindAllDisAppearedInAnArray.cs
+++ Tutor/Arrays/FindAllDisAppearedInAnArray.cs
@@ -10,8 +10,33 @@ namespace Tutor.Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("FindAllDisAppearedInAnArray");
-           
+            int[] nums = { 4,3,2,7,8,2,3,1}; // 5 && 6
+            Console.WriteLine(BruteForceApproach(nums));
+        }
+
+        public static IList<int> BruteForceApproach(int[] nums)
+        {
+            List<int> result = new List<int>();
+           // 1st step -> need to run the loop from 1 - range
+           for (int i = 1; i <= nums.Length; i++)
+            {
+                bool b = false;
+
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] == i)
+                    {
+                        b= true;
+                        break;
+                    }
+                }
+                if (b == false)
+                {
+                    result.Add(i);
+                }
+            }
+           return result;
+
         }
     }
 }
