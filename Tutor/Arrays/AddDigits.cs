48b6495c163cffbced85ea1a64f78a29511f32ba
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-26 10:56:59 +0530

    Leetcode-Number : 258

---
+++ Tutor/Arrays/AddDigits.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Arrays
+{
+    internal class AddDigits
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        private static int OptimizedOne(int num)
+        {
+            if (num < 0) return 0;
+        }
+    }
+}
