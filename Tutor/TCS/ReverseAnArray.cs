0bd61a8adb1e7ac967f7e5757aebacfa41925a32
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-28 17:40:22 +0530

    Reverse of An Array

---
+++ Tutor/TCS/ReverseAnArray.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.TCS
+{
+    internal class ReverseAnArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1,2,3,4,5};
+            ReverseArray(arr);
+        }
+
+        private static void ReverseArray(int[] array)
+        {
+            int n = array.Length;
+
+            for (int i = n-1; i >= 0; i--)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
