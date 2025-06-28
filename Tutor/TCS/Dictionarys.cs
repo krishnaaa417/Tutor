ad9a01de8245a5e01f20545f435517176e653bfe
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-28 22:52:25 +0530

    Frequencies of each element

---
+++ Tutor/TCS/Dictionarys.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.TCS
+{
+    internal class Dictionarys
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+
+            dict.Add(1, "Nallapothula");
+            dict.Add(2, "Venkata");
+            dict.Add(3, "Krishna");
+            dict.Add(4, "Prasad");
+
+            Console.WriteLine(dict.Count);
+            Console.WriteLine(dict.ContainsKey(3));
+
+            foreach (var result in dict)
+            {
+                Console.WriteLine(result);
+            }
+
+            dict[1] = "love";
+
+            Console.WriteLine("--");
+            foreach (var result in dict)
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine("---");
+
+            if (dict.ContainsKey(4))
+            {
+                Console.WriteLine("elses");
+            }
+
+
+        }
+    }
+}
---
+++ Tutor/TCS/FrequencyOfEachElement.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.TCS
+{
+    internal class FrequencyOfEachElement
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 7, 2, 4, 8, 7, 4, 9, 2 };
+            Dictionary<int, int> map = Frequency(arr);
+            Console.WriteLine("frequencies");
+
+            foreach (var entry in map)
+            {
+                Console.WriteLine("number."+entry.Key +",,"+entry.Value);
+            }
+
+        }
+
+        private static Dictionary<int,int> Frequency(int[] nums)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            foreach (int number in nums)
+            {
+                if (map.ContainsKey(number))
+                {
+                    map[number]++;
+                }
+                else
+                {
+                    map.Add(number, 1);
+                }
+            }
+            return map;
+        }
+    }
+}
