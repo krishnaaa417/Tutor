ddc89c308a2462cf87460ef495e8f947684615a4
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-28 16:47:44 +0530

    second Largestnumber in the given array

--- Tutor/TCS/LargestNumber.cs
+++ Tutor/TCS/LargestNumber.cs
@@ -11,7 +11,7 @@ namespace Tutor.TCS
         static void Main(string[] args)
         {
             int[] nums = { 22, 341, 32, 0, 1 ,9030};
-            Console.WriteLine("Largest-Number in the array.." + LargestNumberIntheGivenArray(nums));
+            Console.WriteLine("Largest-Number in the array.." + SecondLargestNumber(nums));
         }
 
         private static int LargestNumberIntheGivenArray(int[] nums)
@@ -27,6 +27,28 @@ namespace Tutor.TCS
                 }
             }
             return min;
+        }
+
+        private static int SecondLargestNumber(int[] nums)
+        {
+            
+            int n = nums.Length;
+            int largest = int.MinValue;
+            int secondlargest = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] > largest)
+                {
+                    secondlargest = largest;
+                    largest = nums[i];
+                }
+                else if (secondlargest < largest && nums[i] > largest)
+                {
+                    secondlargest = nums[i];
+                }
+            }
+            return secondlargest;
         }
     }
 }
