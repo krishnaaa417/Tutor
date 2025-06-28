99dbd72ccb8d0340ac401c79d4ee9af52c564fc8
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-28 16:06:55 +0530

    Largest number in the array

---
+++ Tutor/TCS/LargestNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.TCS
+{
+    internal class LargestNumber
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 22, 341, 32, 0, 1 ,9030};
+            Console.WriteLine("Largest-Number in the array.." + LargestNumberIntheGivenArray(nums));
+        }
+
+        private static int LargestNumberIntheGivenArray(int[] nums)
+        {
+            int n = nums.Length;
+            int min = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (nums[i] > min)
+                {
+                    min = nums[i];
+                }
+            }
+            return min;
+        }
+    }
+}
