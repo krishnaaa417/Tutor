09eb9837866bac73b5f2cfd36ac78f8d221c4338
krishnaaa210 <krishnaaa210@gmail.com>
2025-06-30 23:11:38 +0530

    LeetCode number : 682

---
+++ Tutor/TCS/BaseBallGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.TCS
+{
+    internal class BaseBallGame
+    {
+        static void Main(string[] args)
+        {
+            string[] s = { "5", "-2", "4", "C", "D", "9", "+", "+" };
+
+        }
+
+        private static int BruteForceApproach(string[] s)
+        {
+            int n = s.Length;
+            // + = sum of previous two scores;
+            // d = double of previous one
+            //c = remove the one
+            // finally return sum of all fucking numbers
+
+            
+
+            return s.Length;
+        }
+    }
+}
