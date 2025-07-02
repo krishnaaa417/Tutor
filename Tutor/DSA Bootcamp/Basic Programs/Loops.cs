9158e64b593e379292ffd7d18bccc1f27ab77501
krishnaaa210 <krishnaaa210@gmail.com>
2025-07-02 08:20:09 +0530

    Bootcamp Day 0

---
+++ Tutor/DSA Bootcamp/Basic Programs/Loops.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.DSA_Bootcamp.Basic_Programs
+{
+    internal class Loops
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number..");
+            int number = Convert.ToInt32(Console.ReadLine());
+            PrintingNumber(number);
+
+        }
+
+        private static void PrintingNumber(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Tutor/DSA Bootcamp/Basic Programs/WhileLoop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.DSA_Bootcamp.Basic_Programs
+{
+    internal class WhileLoop
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number.");
+            int number = Convert.ToInt32(Console.ReadLine());
+            EvenNumber(number);
+        }
+
+        private static void EvenNumber(int number)
+        {
+            while (number > 0)
+            {
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine(number);
+                }
+                number++;
+            }
+        }
+    }
+}
