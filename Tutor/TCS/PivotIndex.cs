5d291b71f50fe58ba52d43225fa1daf5c6129bda
krishnaaa210 <krishnaaa210@gmail.com>
2025-07-03 20:57:23 +0530

    leetcode number: 724

---
+++ Tutor/TCS/PivotIndex.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.TCS
+{
+    internal class PivotIndex
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 7, 3, 6, 5, 6 };
+            Console.WriteLine(OptimisedApproach(nums));
+
+        }
+
+        private static int OptimisedApproach(int[] nums)
+        {
+            int n = nums.Length;
+            int totalsum = nums.Sum();
+            int leftsum = 0;
+           for (int i = 0; i < n; i++)
+            {
+                int rightsum = totalsum - leftsum - nums[i];
+                if ((rightsum == leftsum))
+                
+                    return i;
+                leftsum = leftsum + nums[i];
+            }
+            return -1;
+        }
+    }
+}
