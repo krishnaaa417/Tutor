f83af6b2de0e5d08286a3e4b5d5d2fb5987b98d1
krishnaaa210 <krishnaaa210@gmail.com>
2025-07-05 13:40:12 +0530

    Linked List Data Structure

---
+++ Tutor/LinkedList/BasicOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.LinkedList
+{
+    internal class BasicOperations
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static void PrimaryOperations()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddFirst(1);
+            list.AddFirst(2);
+            list.AddLast(3);
+            list.AddLast(4);
+
+            
+        }
+    }
+}
