315c1d93198b6bff5c891de65f3547eb32f10366
krishnaaa210 <krishnaaa210@gmail.com>
2025-07-08 10:48:13 +0530

    Traverse the LinkedList

---
+++ Tutor/LinkedList/Traverse.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.LinkedList
+{
+    internal class Traverse
+    {
+        static void Main(string[] args)
+        {
+            Node node3 = new Node(30);
+            Node node2 = new Node(20, node3);
+            Node head = new Node(10, node2);
+            TraverseList(head);
+        }
+
+        static void TraverseList(Node head)
+        {
+            while (head != null)
+            {
+                Console.WriteLine(head.val+",");
+                head = head.next;
+            }
+            Console.WriteLine("null..");
+        }
+    }
+
+    public class Node
+    {
+        public int val;
+        public Node next;
+
+        public Node(int value)
+        {
+            val = value;
+        }
+        public Node(int value,Node nexts)
+        {
+            val = value;
+            next = nexts;
+        }
+    }
+}
