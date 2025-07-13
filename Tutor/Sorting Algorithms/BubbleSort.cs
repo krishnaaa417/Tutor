5940322deed366e73011a54081a3cecc89c988b7
krishnaaa210 <krishnaaa210@gmail.com>
2025-07-13 13:31:30 +0530

    Bubble sort BruteForce approach done

--- Tutor/Sorting Algorithms/BubbleSort.cs
+++ Tutor/Sorting Algorithms/BubbleSort.cs
@@ -10,7 +10,33 @@ namespace Tutor.Sorting_Algorithms
     {
         static void Main(string[] args)
         {
+            int[] nums = { 4, 5, 1, 3, 2 };
+            var result = BruteForceApproach(nums);
+
+            foreach (int i in nums)
+            {
+                Console.WriteLine(i);
+            }
             
+        }
+
+        private static int[] BruteForceApproach(int[] nums)
+        {
+            int n = nums.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        int temp = nums[i];
+                        nums[i]= nums[j];
+                        nums[j]= temp;
+                    }
+                }
+                
+            }
+            return nums;
         }
     }
 }
