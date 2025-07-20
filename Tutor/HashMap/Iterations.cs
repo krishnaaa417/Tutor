86caa1bf1f13479b4af381f80929544c1ec8749c
krishnaaa210 <krishnaaa210@gmail.com>
2025-07-20 13:41:14 +0530

    Hashing Concept

---
+++ Tutor/HashMap/Iterations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.HashMap
+{
+    internal class Iterations
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String,int> dict = new Dictionary<String,int>();
+            dict.Add("Mango", 1);
+            dict.Add("Apple", 2);
+            dict.Add("Lemon", 3);
+
+            foreach (var dic in dict)
+            {
+                Console.WriteLine(dic.Key +","+dict.Values);
+            }
+        }
+    }
+}
