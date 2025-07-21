df61176859dcbb99990ec6f3db96c70f9fb60c89
krishnaaa210 <krishnaaa210@gmail.com>
2025-07-21 09:54:25 +0530

    brute force approach

---
+++ Tutor/HashMap/CountOfFrequencies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.HashMap
+{
+    internal class CountOfFrequencies
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1,2,3,1,2,2};
+            int n = nums.Length;
+            bool[] filled = new bool[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (filled[i] == true) { continue; }
+
+                int count = 0;
+
+                for (int j = 0; j < n; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        count++;
+                        filled[j]   = true;
+                    }
+                }
+
+                Console.WriteLine($"Element {nums[i]} occurs {count} times");
+            }
+        }
+
+        
+    }
+}
