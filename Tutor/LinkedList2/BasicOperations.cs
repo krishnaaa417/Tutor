08cce6a3fa0faaa2763dc7fe2d3b09636859b3c5
krishnaaa210 <krishnaaa210@gmail.com>
2025-08-01 19:43:46 +0530

    Linked List

---
+++ Tutor/LinkedList2/BasicOperations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.LinkedList2
+{
+    internal class BasicOperations
+    {
+        static void Main(string[] args)
+        {
+            Node n1 = new Node(10);
+            Node n2 = new Node(20);
+            Node n3 = new Node(30);
+            Node n4 = new Node(40);
+
+            n1.next = n2;
+            n2.next = n3;
+            n3.next = n4;
+            Node head = n1;
+
+          while (head != null)
+            {
+
+                Console.WriteLine(head.value);
+                head = head.next;
+               
+            }
+        }
+    }
+
+
+    public class Node
+    {
+        public int value;
+        public Node next;
+
+        public Node(int value)
+        {
+                this.value = value;
+                this.next = null;
+        }
+
+    }
+}
---
+++ Tutor/LinkedList2/ReverseLinkedList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.LinkedList2
+{
+    internal class ReverseLinkedList
+    {
+        static void Main(string[] args)
+        {
+           ListNode l1 = new ListNode(10);
+            ListNode l2 = new ListNode(20);
+            ListNode l3 = new ListNode(30);
+            ListNode l4 = new ListNode(40);
+            ListNode l5 = new ListNode(50);
+            ListNode head = l1;
+            l1.next = l2;
+            l2.next = l3;
+            l3.next = l4;
+            Console.WriteLine("original list:");
+            PrintList(l1);
+            ListNode reversed = ReverseList(l1);
+
+            Console.WriteLine("ReversedList..");
+            PrintList(reversed);
+        }
+
+        public static ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode current = head;
+
+            while (current != null)
+            {
+                ListNode nextnode = current.next;
+                current.next = prev;
+                prev = current;
+                current = nextnode;
+            }
+            return prev;
+        }
+        public static void PrintList(ListNode head)
+        {
+            ListNode current = head;
+            while (current != null)
+            {
+                Console.Write(current.value + " -> ");
+                current = current.next;
+            }
+            Console.WriteLine("null");
+        }
+    }
+    public class ListNode
+    {
+       public int value; //instance variable
+       public ListNode next;
+
+        public ListNode(int value=0,ListNode next = null)
+        {
+            this.value = value;
+            this.next = next;
+        }
+    }
+}
