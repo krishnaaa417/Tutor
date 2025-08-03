30c06306c8f8843e246c83187e2cf941f12318ac
krishnaaa210 <krishnaaa210@gmail.com>
2025-08-03 08:46:12 +0530

    Dynamic Programming started

---
+++ Tutor/Dynamic Programming/Recursion/PrintNumbers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Dynamic_Programming.Recursion
+{
+    internal class PrintNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OptimizedApproach(5));
+        }
+
+        private static int OptimizedApproach(int n)
+        {
+            //Base condition
+            if (n == 0) return n;
+
+            Console.WriteLine("value.."+n);
+
+            return OptimizedApproach(n - 1);
+
+        }
+    }
+}
---
+++ Tutor/PriorityQueue/KthLargestElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.PriorityQueue
+{
+    internal class KthLargestElement
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+
+        
+    }
+
+
+}
