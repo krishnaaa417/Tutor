7bdc0303fac0e0462dcdf7b71aa7529664cb7969
krishnaaa210 <krishnaaa210@gmail.com>
2025-08-04 12:27:20 +0530

    By using recursions

---
+++ Tutor/Dynamic Programming/Recursion/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Dynamic_Programming.Recursion
+{
+    internal class Factorial
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter any number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Fact(n));
+        }
+        private static int Fact(int n)
+        {
+            if (n == 1) return 1;
+            return Fact(n - 1) * n;
+        }
+    }
+}
---
+++ Tutor/Dynamic Programming/Recursion/SumOfFirstNnaturalNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.Dynamic_Programming.Recursion
+{
+    internal class SumOfFirstNnaturalNumbers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number..");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("---");
+            Console.WriteLine(BruteForce(n));
+            Console.WriteLine("--Recursion below..");
+            Console.WriteLine(usingRecursion(n)); // Updated to correctly call the recursive method  
+        }
+
+        private static int BruteForce(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+
+        private static int usingRecursion(int n) 
+        {
+            if (n == 0) return 0;
+            return n+usingRecursion(n-1);
+        }
+    }
+}
