af5da0703fff686761c7a3320fb6ec29cb50144c
krishnaaa210 <krishnaaa210@gmail.com>
2025-12-31 22:56:58 +0530

    Fibonacci number

---
+++ Tutor/BasicLoops/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.BasicLoops
+{
+    internal class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            for(int i=0; i <= 10; i++)
+            {
+                int c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
