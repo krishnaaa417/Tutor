659fbe09ee29dd0b23238bbe7648186965d8fce7
krishnaaa210 <krishnaaa210@gmail.com>
2025-12-31 23:05:21 +0530

    Factorial.

---
+++ Tutor/BasicLoops/Factorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutor.BasicLoops
+{
+    internal class Factorial
+    {
+        static void Main(string[] args)
+        {
+
+            int fact = 1;
+            for(int i = 5; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+        }
+    }
+}
